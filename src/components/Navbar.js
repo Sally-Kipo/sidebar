e9747e5f6eb3e301fd389adca9c65954c3843c8d
sally-kipo <saliyipiani@gmail.com>
2024-09-20 15:16:34 +0400

    Your message

---
+++ src/components/Navbar.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import './Navbar.css'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import CallRoundedIcon from '@mui/icons-material/CallRounded';
+import MailRoundedIcon from '@mui/icons-material/MailRounded';
+const Navbar = () => {
+    const [state, setState] = React.useState({
+      top: false,
+      left: false,
+      bottom: false,
+      right: false,
+    });
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+      const list = (anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))} */}
+          </List>
+          <Divider />
+          <List>
+            {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))} */}
+            <ListItem disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                    <HomeRoundedIcon/>
+                    </ListItemIcon>
+                <ListItemText primary="Home"/>
+            </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+            <ListItemButton>
+                    <ListItemIcon>
+                    <InfoRoundedIcon/>
+                    </ListItemIcon>
+                <ListItemText primary="About"/>
+            </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+            <ListItemButton>
+                    <ListItemIcon>
+                    <CallRoundedIcon/>
+                    </ListItemIcon>
+                <ListItemText primary="Contact"/>
+            </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+            <ListItemButton>
+                    <ListItemIcon>
+                    <MailRoundedIcon/>
+                    </ListItemIcon>
+                <ListItemText primary="Inbox"/>
+            </ListItemButton>
+            </ListItem>
+          </List>
+          
+        </Box>
+      );
+      return (
+        <div className='Navbar'>
+          {/* {['left', 'right', 'top', 'bottom'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+              <Drawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+              >
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))} */}
+          <React.Fragment>
+            <MenuIcon 
+            onClick ={
+                toggleDrawer("left", true)
+            }
+            />
+            <Drawer
+                anchor={"left"}
+                open={state["left"]}
+                onClose={toggleDrawer("left", false)}
+              >
+                {list("left")}
+              </Drawer>
+          </React.Fragment>
+        </div>
+      );
+    }
+
+export default Navbar
